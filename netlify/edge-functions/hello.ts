825060c47464bcb1b2dc937e00ed223538b4c707
Silent Sybber <silentsybber@gmail.com>
2022-06-01 09:50:54 +0200

    tests edge functions

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,7 +1,7 @@
 import { Context } from 'netlify:edge';
 
-export default async (req: Request, { log }: Context) => {
+export default async (req: Request, { log, json }: Context) => {
 	log('Processing request for', req.url);
 
-	return new Response('Hello world');
+	return json('Hello world');
 };
