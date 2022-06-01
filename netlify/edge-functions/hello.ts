405fa8d8106c2aed375baeb2d12056d68f7e52dd
Silent Sybber <silentsybber@gmail.com>
2022-06-01 09:56:06 +0200

    remove native edge functions

--- netlify/edge-functions/hello.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from 'netlify:edge';
-
-export default async (req: Request, { log, json }: Context) => {
-	log('Processing request for', req.url);
-
-	return json('Hello world');
-};
