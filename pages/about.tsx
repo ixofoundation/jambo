8a11cd198157b6aa34853f0782a21f6a5f4cf66c
Michael Pretorius <michael@ixo.world>
2022-10-28 14:41:46 +0200

    fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,11 +19,14 @@ const About: NextPage = () => {
 			<Header />
 
 			<main className={cls(utilsStyles.main, utilsStyles.columnJustifyCenter, styles.about)}>
+				<div className={utilsStyles.spacerFlex} />
 				<h2 className={styles.title}>About</h2>
 				<p className={styles.text}>{config.about}</p>
 				{/* <h2 className={styles.title}>Account History</h2> */}
 
 				<Footer onBackUrl="/" />
+				<div className={utilsStyles.spacerFlex} />
+				<div className={utilsStyles.spacerFlex} />
 			</main>
 		</>
 	);
