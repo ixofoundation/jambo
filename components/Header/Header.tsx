ecd765a048f8e7932dff4249554b9308bc9cfe7a
Pieter Pretorius <pieter.pretorius@ixo.world>
2023-03-08 15:17:06 +0200

    feat: showing logo and name during initial loading

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,14 +8,15 @@ import VerticalDots from '@icons/vertical_dots.svg';
 import DappStore from '@icons/squares_grid.svg';
 import { pushNewRoute } from '@utils/router';
 import config from '@constants/config.json';
+import { FC } from 'react';
 
 type HeaderProps = {
   configure?: boolean;
   header?: string;
 };
 
-const Header = ({ configure = false, header }: HeaderProps) => {
-  const { headerShowLogo, headerShowName, siteName } = config;
+const Header: FC<HeaderProps> = ({ configure = false, header }) => {
+  const { headerShowLogo, headerShowName } = config;
   const router = useRouter();
 
   return (
@@ -23,18 +24,38 @@ const Header = ({ configure = false, header }: HeaderProps) => {
       <Anchor active openInNewTab href='https://my.jambo.earth/'>
         <ColoredIcon icon={DappStore} color={ICON_COLOR.iconGrey} size={25} className={styles.headerIcon} />
       </Anchor>
-      <div className={styles.row} onClick={() => (configure ? null : pushNewRoute('/'))}>
-        {!header && headerShowLogo && (
-          <div className={styles.logo}>
-            <Image alt='logo' src='/images/logo.png' layout='fill' priority />
-          </div>
-        )}
-        {(header || headerShowName) && <h1 className={styles.name}>{header ?? siteName}</h1>}
-      </div>
+      <SiteHeader
+        displayLogo={!header && headerShowLogo}
+        displayName={!!(header || headerShowName)}
+        name={header}
+        onClick={() => (configure ? null : pushNewRoute('/'))}
+      />
       <Anchor active href='/settings'>
         <ColoredIcon icon={VerticalDots} color={ICON_COLOR.iconGrey} size={25} className={styles.headerIcon} />
       </Anchor>
     </nav>
+  );
+};
+
+type SiteHeaderProps = {
+  displayLogo?: boolean;
+  displayName?: boolean;
+  name?: string;
+  onClick?: () => void;
+};
+
+export const SiteHeader: FC<SiteHeaderProps> = ({ displayLogo, displayName, name, onClick }) => {
+  const { siteName } = config;
+
+  return (
+    <div className={styles.row} onClick={onClick}>
+      {displayLogo && (
+        <div className={styles.logo}>
+          <Image alt='logo' src='/images/logo.png' layout='fill' priority />
+        </div>
+      )}
+      {displayName && <h1 className={styles.name}>{name ?? siteName}</h1>}
+    </div>
   );
 };
 
