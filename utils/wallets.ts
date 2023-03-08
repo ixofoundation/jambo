dfb872dcb8f3152ebc09be9ebbda6765373d6b17
Pieter Pretorius <pieter.pretorius@ixo.world>
2023-03-08 12:39:03 +0200

    fix: adding fallbacks for actions and wallets

--- utils/wallets.ts
+++ utils/wallets.ts
@@ -17,7 +17,6 @@ import { customQueries } from '@ixo/impactxclient-sdk';
 export const shortenAddress = (address: string) =>
   (address?.length && address.length > 19 ? address.slice(0, 12).concat('...').concat(address.slice(-7)) : address) ??
   '';
-// TODO: replace next/Image with ImageWithFallback everywhere
 
 // TODO: provide denom as 5th param to only group for the denom
 export const groupWalletAssets = (
