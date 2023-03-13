7c3756e4a3482ad98a61556284f9d9d01f525a95
Pieter Pretorius <pieter.pretorius@ixo.world>
2023-03-13 15:16:03 +0200

    fix: fixing loader and adding edit icon

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,15 +1,23 @@
 import { HTMLAttributes } from 'react';
-import { TailSpin } from 'react-loader-spinner';
-import { getCSSVariable } from '@utils/styles';
+// import { TailSpin } from 'react-loader-spinner';
+// import { getCSSVariable } from '@utils/styles';
+
+import styles from './Loader.module.scss';
 
 type LoaderProps = {
   size?: number;
 } & HTMLAttributes<HTMLDivElement>;
 
 const Loader = ({ size = 50, className, ...other }: LoaderProps) => {
-  const primaryColor = getCSSVariable('--primary-color');
+  // const primaryColor = getCSSVariable('--primary-color');
 
-  return <TailSpin height={size} width={size} color={primaryColor ?? '#000'} wrapperClass={className} />;
+  return (
+    <div
+      className={styles.tailSpin}
+      style={{ width: size, height: size, borderWidth: 0.1 * size < 2 ? 2 : 0.1 * size }}
+    />
+  );
+  // return <TailSpin height={size} width={size} color={primaryColor ?? '#000'} wrapperClass={className} />;
 };
 
 export default Loader;
