8f3a0ad38b71399adec3102a9c30524cb9ed62ef
Rafeeq Weideman <r.w.weideman.rafeeq@gmail.com>
2023-04-14 14:56:48 +0100

    inprogress: VoteAction

--- steps/Proposals.tsx
+++ steps/Proposals.tsx
@@ -2,8 +2,6 @@ import React, { FC, useState, useEffect } from 'react';
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
 import { StepDataType, STEPS } from 'types/steps';
-import { Proposal } from '@ixo/impactxclient-sdk/types/codegen/cosmos/gov/v1beta1/gov';
-import { VoteOption } from '@ixo/impactxclient-sdk/types/codegen/cosmos/gov/v1beta1/gov';
 import axios from 'axios';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.min.css';
@@ -18,6 +16,8 @@ const api = axios.create({
 
 type GetProposalsProps = {
     data?: StepDataType<STEPS.select_and_review_proposal>;
+    proposalId: string;
+    proposal: Proposal[];
 }
 
 const BtnStyles = {
@@ -35,9 +35,24 @@ const VoteBtns = {
     borderRadius: '100%'
 }
 
-const Proposals: FC<GetProposalsProps> = ({ data }) => {
+interface Proposal {
+    id: number;
+    proposal_id: string;
+}
+
+const Proposals: FC<GetProposalsProps> = () => {
     const [proposals, setProposals] = useState<Proposal[]>([]);
     const [selected, setSelected] = useState(false);
+    const [selectedValue, setSelectedValue] = useState(false);
+
+    const handleSelect = (proposal_id: number) => {
+        const selectedProposal = proposals.find((proposal) => proposal.id === proposal_id);
+        if (selectedProposal) {
+            setSelectedValue(!selectedValue);
+        } else {
+            console.log(`${proposal_id}`);
+        }
+    }
 
     useEffect(() => {
         const fetchProposals = async () => {
@@ -49,7 +64,6 @@ const Proposals: FC<GetProposalsProps> = ({ data }) => {
                 console.error(error);
             }
         };
-
         fetchProposals();
     }, [])
 
@@ -62,12 +76,6 @@ const Proposals: FC<GetProposalsProps> = ({ data }) => {
         const yesPercentage = totalVotes ? (yesVotes / (totalVotes - proposals.final_tally_result.abstain)) * 100 : 0;
         // Update the progress bar using the yesPercentage value
     }
-
-
-
-
-    // Update the progress bar using the yesPercentage value
-
 
     return (
         <div className="div">
@@ -93,6 +101,8 @@ const Proposals: FC<GetProposalsProps> = ({ data }) => {
                             {proposals.map((proposal) => (
                                 <SwiperSlide
                                     key={proposal.proposal_id}
+                                    onClick={() => handleSelect(proposal.proposal_id)}
+                                    // className={selectedValue === proposal.proposal_id ? 'selected' : ''}
                                     style={{
                                         backgroundColor: '#EBEBEB',
                                         height: selected ? '400px' : '300px',
@@ -102,12 +112,15 @@ const Proposals: FC<GetProposalsProps> = ({ data }) => {
                                         justifyContent: 'center',
                                         borderRadius: '15px',
                                         overflow: 'hidden',
+                                        borderStyle: selectedValue ? 'solid' : '',
+                                        borderColor: 'lightblue'
                                     }}
+
                                 >
                                     <div className="div">
                                         <h1 style={{ fontSize: '11px', textAlign: 'left' }} >{proposal.content.title}</h1>
 
-                                        <div style={{ width: '100%', height: '20px', backgroundColor: '#f0f0f0', borderRadius: '10px' }}>
+                                        <div style={{ width: '100%', height: '5px', backgroundColor: '#f0f0f0', borderRadius: '10px' }}>
                                             <div style={{ width: `${yesPercentage}%`, height: '100%', backgroundColor: 'blue', borderRadius: '10px' }}></div>
                                         </div>
 
@@ -119,13 +132,22 @@ const Proposals: FC<GetProposalsProps> = ({ data }) => {
                                                 overflow: 'hidden',
                                                 textAlign: 'left',
                                                 width: '16rem',
-                                                overflowY: 'auto'
+                                                overflowY: 'auto',
                                             }}
                                         >
                                             {proposal.content.description}
                                         </p>
                                         <div
-                                            style={{ display: 'flex', justifyContent: 'space-evenly' }}
+                                            style={{
+
+                                                display: 'flex',
+                                                justifyContent: 'space-evenly',
+                                                alignItems: 'center',
+                                                position: 'relative',
+                                                height: '70px',
+                                                bottom: '50px',
+                                                backgroundColor: '#EBEBEB'
+                                            }}
                                         >
                                             <button style={BtnStyles} onClick={() => setSelected(!selected)} >Actions</button>
                                             <button style={BtnStyles} >Resources</button>
@@ -141,7 +163,6 @@ const Proposals: FC<GetProposalsProps> = ({ data }) => {
                     }
                 </Swiper >
             </div>
-
             {
                 selected ? (
                     <div
@@ -150,7 +171,7 @@ const Proposals: FC<GetProposalsProps> = ({ data }) => {
                             justifyContent: 'space-evenly',
                             position: 'fixed',
                             width: '100%',
-                            bottom: '20px'
+                            bottom: '20px',
                         }}
                     >
                         <button style={VoteBtns}>YES</button>
