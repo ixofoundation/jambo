99d924e5e24b2b9e7c961318cfb89f5d28a91b71
Shakalov <yaroslav.shakalov@sigma.software>
2023-08-21 21:09:15 +0300

    return examples

--- constants/pools.ts
+++ constants/pools.ts
@@ -1,13 +1,15 @@
 import { Dictionary, Pool, Token, TokenType } from 'types/swap';
 
 export const tokens = new Map<string, Token>();
-tokens.set('USDT', { address: 'ixo1r4azksxfmfn3wx6tlazcu5acreymnvyacnu3q33532zdt6ypwmxqnystvl', type: TokenType.Cw20 });
-tokens.set('CARBON', {
-  address: 'ixo1aakfpghcanxtc45gpqlx8j3rq0zcpyf49qmhm9mdjrfx036h4z5skn3d4n',
-  type: TokenType.Cw1155,
-});
-tokens.set('uixo', { type: TokenType.Native });
+// Example
+// tokens.set('USDT', { address: 'ixo15d2tyq2jzxmpg32y3am3w62dts32qgzmds9qnr6c87r0gwwr7ynq28958z', type: TokenType.Cw20 });
+// tokens.set('CARBON', {
+//   address: 'ixo15d2tyq2jzxmpg32y3am3w62dts32qgzmds9qnr6c87r0gwwr7ynq28958z',
+//   type: TokenType.Cw1155,
+// });
+// tokens.set('uixo', { type: TokenType.Native });
 
 export const pools = new Dictionary((keyObject: Pool) => JSON.stringify(keyObject));
-pools.set({ token1155: 'CARBON', token2: 'USDT' }, 'ixo123321');
-pools.set({ token1155: 'CARBON', token2: 'uixo' }, 'ixo1sjfach0vqaz5dyqum9e5wk7jesltpdugewzhz94xvs5ayt28dnyqhlxzs8');
+// Example
+// pools.set({ token1155: 'CARBON', token2: 'USDT' }, 'ixo15d2tyq2jzxmpg32y3am3w62dts32qgzmds9qnr6c87r0gwwr7ynq28958z');
+// pools.set({ token1155: 'CARBON', token2: 'uixo' }, 'ixo15d2tyq2jzxmpg32y3am3w62dts32qgzmds9qnr6c87r0gwwr7ynq28958z');
