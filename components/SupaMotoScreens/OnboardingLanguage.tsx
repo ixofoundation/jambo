dfea0ff65ee9898194f019c5e9bb0d185e2a35a6
RafeeqWeideman-YDS <rafeeq.weideman@yds.solutions>
2023-09-14 09:12:53 +0200

    Updated: OnboardingLanguage.tsx

--- components/SupaMotoScreens/OnboardingLanguage.tsx
+++ components/SupaMotoScreens/OnboardingLanguage.tsx
@@ -1,36 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SupaMotoScreens.module.scss'
-
 import Language from '@icons/language.svg';
 import IconText from '@components/IconText/IconText';
 
 const OnboardingLanguage = () => {
+    const [selected, setSelected] = useState<string | null>(null);
+
+    const handleButtonClick = (label: string) => {
+        if (selected === label) {
+            setSelected(null);
+        } else {
+            setSelected(label);
+        }
+    };
+
+    const isButtonSelected = (label: string) => {
+        return selected === label;
+    };
+
     return (
-        <div>
+        <div className={styles.onboardingComponent} >
             <IconText title='Onboarding Languages' Img={Language} imgSize={50} />
             <table>
                 <tr>
                     <button
-                        className={styles.language_button}
+                        className={`${styles.language_button} ${isButtonSelected('Bemba') ? styles.selected : ''}`}
+                        onClick={() => handleButtonClick('Bemba')}
                     >Bemba</button>
                     <button
-                        className={styles.language_button}
+                        className={`${styles.language_button} ${isButtonSelected('Nyanja') ? styles.selected : ''}`}
+                        onClick={() => handleButtonClick('Nyanja')}
                     >Nyanja</button>
                 </tr>
                 <tr>
                     <button
-                        className={styles.language_button}
+                        className={`${styles.language_button} ${isButtonSelected('Lozi') ? styles.selected : ''}`}
+                        onClick={() => handleButtonClick('Lozi')}
                     >Lozi</button>
                     <button
-                        className={styles.language_button}
+                        className={`${styles.language_button} ${isButtonSelected('Tonga') ? styles.selected : ''}`}
+                        onClick={() => handleButtonClick('Tonga')}
                     >Tonga</button>
                 </tr>
                 <tr>
                     <button
-                        className={styles.language_button}
+                        className={`${styles.language_button} ${isButtonSelected('Kaonde') ? styles.selected : ''}`}
+                        onClick={() => handleButtonClick('Kaonde')}
                     >Kaonde</button>
                     <button
-                        className={styles.language_button}
+                        className={`${styles.language_button} ${isButtonSelected('English') ? styles.selected : ''}`}
+                        onClick={() => handleButtonClick('English')}
                     >English</button>
                 </tr>
             </table>
