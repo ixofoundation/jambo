a8f0d2e51389cf3501ce12e216ee1da24a3a1cc4
Pieter Pretorius <pieter.pretorius@ixo.world>
2023-11-01 14:39:12 +0200

    feat: adding chain to chainInfo

--- types/chain.ts
+++ types/chain.ts
@@ -2,7 +2,9 @@ import { ChainNetwork } from '@ixo/impactxclient-sdk/types/custom_queries/chain.
 import { KeplrChainInfo, RegistryChainInfo } from '@ixo/cosmos-chain-resolver/types/types/chain';
 
 export type REGISTRY_CHAIN_INFO_TYPE = RegistryChainInfo;
-export type KEPLR_CHAIN_INFO_TYPE = KeplrChainInfo;
+export type KEPLR_CHAIN_INFO_TYPE = KeplrChainInfo & {
+  chainNetwork?: CHAIN_NETWORK_TYPE;
+};
 
 export type CHAIN_NETWORK_TYPE = ChainNetwork;
 
