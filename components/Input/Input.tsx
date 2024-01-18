24e10af0332165b68387a5139d9414f21ab55067
afika <afikashwashwa@gmail.com>
2024-01-18 13:28:17 +0200

    Input validation updated

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,4 +1,85 @@
-import { FC, InputHTMLAttributes } from 'react';
+// import { FC, InputHTMLAttributes } from 'react';
+// import cls from 'classnames';
+
+// import styles from './Input.module.scss';
+// import { CURRENCY_TOKEN } from 'types/wallet';
+// import {
+//   calculateMaxTokenAmount,
+//   formattedAmountToNumber,
+//   getAmountFromCurrencyToken,
+//   getDecimalsFromCurrencyToken,
+//   getDisplayDenomFromCurrencyToken,
+// } from '@utils/currency';
+
+// type InputProps = {
+//   label?: string;
+//   align?: 'start' | 'left' | 'center' | 'right' | 'end';
+// } & InputHTMLAttributes<HTMLInputElement>;
+
+// const Input: FC<InputProps> = ({ label, align = 'start', className, ...other }) => {
+//   return label ? (
+//     <label className={styles.label}>
+//       {label}
+//       <input
+//         className={cls(
+//           styles.input,
+//           align === 'end' || align === 'right'
+//             ? styles.endAlign
+//             : align === 'center'
+//             ? styles.centerAlign
+//             : styles.startAlign,
+//           className,
+//         )}
+//         {...other}
+//       />
+//     </label>
+//   ) : (
+//     <input
+//       className={cls(
+//         styles.input,
+//         align === 'end' || align === 'right'
+//           ? styles.endAlign
+//           : align === 'center'
+//           ? styles.endAlign
+//           : styles.startAlign,
+//         className,
+//       )}
+//       {... other}
+//     />
+//   );
+// };
+
+// export default Input;
+
+// type InputWithMaxProps = {
+//   maxAmount?: number;
+//   maxToken?: CURRENCY_TOKEN;
+//   onMaxClick: (amount: number) => void;
+// } & InputProps;
+
+// export const InputWithMax = ({ maxAmount, maxToken, onMaxClick, ...other }: InputWithMaxProps) => {
+//   const amount = calculateMaxTokenAmount(
+//     maxAmount ?? getAmountFromCurrencyToken(maxToken),
+//     getDecimalsFromCurrencyToken(maxToken),
+//     true,
+//   );
+//   const denom = getDisplayDenomFromCurrencyToken(maxToken);
+
+//   const handleMaxClick = () => onMaxClick(formattedAmountToNumber(amount));
+
+//   return (
+//     <>
+//       <p className={cls(styles.max, styles.endAlign)} onClick={handleMaxClick}>
+//         {amount} {denom} MAX
+//       </p>
+//       <Input {...other} />
+//     </>
+//   );
+// };
+
+
+// Input.tsx
+import { FC, InputHTMLAttributes, useState } from 'react';
 import cls from 'classnames';
 
 import styles from './Input.module.scss';
@@ -40,11 +121,11 @@ const Input: FC<InputProps> = ({ label, align = 'start', className, ...other }) 
         align === 'end' || align === 'right'
           ? styles.endAlign
           : align === 'center'
-          ? styles.endAlign
+          ? styles.centerAlign
           : styles.startAlign,
         className,
       )}
-      {... other}
+      {...other}
     />
   );
 };
@@ -55,24 +136,46 @@ type InputWithMaxProps = {
   maxAmount?: number;
   maxToken?: CURRENCY_TOKEN;
   onMaxClick: (amount: number) => void;
+  onAmountChange: (amount: number) => void;
 } & InputProps;
 
-export const InputWithMax = ({ maxAmount, maxToken, onMaxClick, ...other }: InputWithMaxProps) => {
+export const InputWithMax = ({ maxAmount, maxToken, onMaxClick, onAmountChange, ...other }: InputWithMaxProps) => {
   const amount = calculateMaxTokenAmount(
     maxAmount ?? getAmountFromCurrencyToken(maxToken),
     getDecimalsFromCurrencyToken(maxToken),
     true,
   );
+  const [inputValue, setInputValue] = useState('');
   const denom = getDisplayDenomFromCurrencyToken(maxToken);
+  const handleMaxClick = () => {
+    setInputValue(amount);
+    onMaxClick(formattedAmountToNumber(amount));
+  };
 
-  const handleMaxClick = () => onMaxClick(formattedAmountToNumber(amount));
+  const change = (event: React.FormEvent<HTMLInputElement>) => {
+    const newValue = Number(event.currentTarget.value);
+    let aamount = Number(amount);
+    if (newValue <= aamount) {
+      setInputValue(event.currentTarget.value);
+    }
+    if (newValue < 0) {
+      setInputValue('0');
+    }
+    //  setInputValue(event.currentTarget.value);
+    console.log(`handleMaxClick called with: ${formattedAmountToNumber(amount)}`);
+    onMaxClick(formattedAmountToNumber(amount));
+    if (onAmountChange) {
+      console.log('Calling onAmountChange with: ', formattedAmountToNumber(amount));
+      onAmountChange(Number(amount));
+    }
+  };
 
   return (
     <>
       <p className={cls(styles.max, styles.endAlign)} onClick={handleMaxClick}>
         {amount} {denom} MAX
       </p>
-      <Input {...other} />
+      <Input {...other} value={inputValue} onInput={change}></Input>
     </>
   );
 };
