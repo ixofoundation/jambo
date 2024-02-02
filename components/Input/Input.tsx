736f5d06378f45388b47f831740eeba456347fbc
afika <afikashwashwa@gmail.com>
2024-02-02 22:20:13 +0200

    send/ multisend proceed button successfully disables until acceptable has been entered.. (validatidation complete)

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,84 +1,4 @@
-// import { FC, InputHTMLAttributes } from 'react';
-// import cls from 'classnames';
 
-// import styles from './Input.module.scss';
-// import { CURRENCY_TOKEN } from 'types/wallet';
-// import {
-//   calculateMaxTokenAmount,
-//   formattedAmountToNumber,
-//   getAmountFromCurrencyToken,
-//   getDecimalsFromCurrencyToken,
-//   getDisplayDenomFromCurrencyToken,
-// } from '@utils/currency';
-
-// type InputProps = {
-//   label?: string;
-//   align?: 'start' | 'left' | 'center' | 'right' | 'end';
-// } & InputHTMLAttributes<HTMLInputElement>;
-
-// const Input: FC<InputProps> = ({ label, align = 'start', className, ...other }) => {
-//   return label ? (
-//     <label className={styles.label}>
-//       {label}
-//       <input
-//         className={cls(
-//           styles.input,
-//           align === 'end' || align === 'right'
-//             ? styles.endAlign
-//             : align === 'center'
-//             ? styles.centerAlign
-//             : styles.startAlign,
-//           className,
-//         )}
-//         {...other}
-//       />
-//     </label>
-//   ) : (
-//     <input
-//       className={cls(
-//         styles.input,
-//         align === 'end' || align === 'right'
-//           ? styles.endAlign
-//           : align === 'center'
-//           ? styles.endAlign
-//           : styles.startAlign,
-//         className,
-//       )}
-//       {... other}
-//     />
-//   );
-// };
-
-// export default Input;
-
-// type InputWithMaxProps = {
-//   maxAmount?: number;
-//   maxToken?: CURRENCY_TOKEN;
-//   onMaxClick: (amount: number) => void;
-// } & InputProps;
-
-// export const InputWithMax = ({ maxAmount, maxToken, onMaxClick, ...other }: InputWithMaxProps) => {
-//   const amount = calculateMaxTokenAmount(
-//     maxAmount ?? getAmountFromCurrencyToken(maxToken),
-//     getDecimalsFromCurrencyToken(maxToken),
-//     true,
-//   );
-//   const denom = getDisplayDenomFromCurrencyToken(maxToken);
-
-//   const handleMaxClick = () => onMaxClick(formattedAmountToNumber(amount));
-
-//   return (
-//     <>
-//       <p className={cls(styles.max, styles.endAlign)} onClick={handleMaxClick}>
-//         {amount} {denom} MAX
-//       </p>
-//       <Input {...other} />
-//     </>
-//   );
-// };
-
-
-// Input.tsx
 import { FC, InputHTMLAttributes, useState } from 'react';
 import cls from 'classnames';
 
@@ -154,20 +74,20 @@ export const InputWithMax = ({ maxAmount, maxToken, onMaxClick, onAmountChange, 
 
   const change = (event: React.FormEvent<HTMLInputElement>) => {
     const newValue = Number(event.currentTarget.value);
-    let aamount = Number(amount);
-    if (newValue <= aamount) {
+    let updatedMaxAmount = Number(amount);
+    if (newValue <= updatedMaxAmount) {
       setInputValue(event.currentTarget.value);
+    
     }
     if (newValue < 0) {
       setInputValue('');
     }
 
-    if (newValue >= aamount) {
+    if (newValue >= updatedMaxAmount) {
       setInputValue('');
     }
 
 
-    //  setInputValue(event.currentTarget.value);
     console.log(`handleMaxClick called with: ${formattedAmountToNumber(amount)}`);
     onMaxClick(formattedAmountToNumber(amount));
     if (onAmountChange) {
