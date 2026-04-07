564908585c830cc2c9e48fbd3d98dbf3bfe0b4c4
Pieter Pretorius | Ixo <pieter.pretorius@ixo.world>
2026-04-07 09:55:34 +0200

    feat: adding claim collection blacklist

--- pages/entities/[entityId]/claimCollections/[collectionId]/index.tsx
+++ pages/entities/[entityId]/claimCollections/[collectionId]/index.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import AuthGuard from '@components/AuthGuard';
 import CollectionDetail from 'screens/collectionDetail';
+import { BLACKLISTED_COLLECTION_IDS } from '@constants/common';
 
 export default function CollectionPage() {
   const router = useRouter();
   const entityDid = router.query.entityId as string | undefined;
   const collectionId = router.query.collectionId as string | undefined;
+
+  const isBlacklisted = collectionId && BLACKLISTED_COLLECTION_IDS.includes(collectionId);
+
+  useEffect(() => {
+    if (isBlacklisted && entityDid) {
+      router.replace(`/entities/${entityDid}`);
+    }
+  }, [isBlacklisted, entityDid, router]);
+
+  if (isBlacklisted) return null;
 
   return (
     <AuthGuard>
